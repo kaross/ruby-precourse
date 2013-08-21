02bf2bb06c9a66deb67b525ecdbde4dc75a6abc2
Kent A Ross <kaross@gmail.com>
2013-08-21 16:56:28 -0400

    chap 6

---
+++ angryboss.rb
@@ -0,0 +1,3 @@
+puts 'What the heck do you want!?'
+resp = gets.chomp.to_s
+puts 'WHADDAYA MEAN "' + resp.upcase + '"!?! YOU\'RE FIRED!!'
---
+++ toc.rb
@@ -0,0 +1,6 @@
+fullwidth = 60
+width = fullwidth/2
+puts('Table of Contents'.center(fullwidth))
+puts('Chapter 1:  Getting Started'.ljust(width) + 'Page 1'.rjust(width))
+puts('Chapter 2:  Numbers'.ljust(width) + 'Page 9'.rjust(width))
+puts('Chapter 3:  Letters'.ljust(width) + 'Page 13'.rjust(width))
